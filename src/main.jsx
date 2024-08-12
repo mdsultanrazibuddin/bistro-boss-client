d45b38835b8fcaf06079cc97759c0e4589af96bc
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-08-12 17:23:48 +0600

    added

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,11 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { router } from './assets/Routes/routes';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
