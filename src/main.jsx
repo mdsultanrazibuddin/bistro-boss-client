3692a8e37b9840089a1cf8fb04a9e064f057b344
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-08-12 17:58:21 +0600

    added

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,9 @@ import { router } from './assets/Routes/routes';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+      <div className='max-w-screen-xl mx-auto'>
+          <RouterProvider router={router} />
+      </div>
+    
   </StrictMode>,
 )
